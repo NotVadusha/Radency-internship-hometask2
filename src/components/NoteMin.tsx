0cb916790f1144317b12886d7c808805dd8ace9d
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-27 23:27:54 +0300

    feat: add notes list and notes minimal components

---
+++ src/components/NoteMin.tsx
@@ -0,0 +1,48 @@
+import { MouseEventHandler } from 'react'
+import { INote } from "../types/INote";
+import { dateFormats } from "../utils/dateFormats";
+import { BsArchive, BsBackspace, BsPencil } from "react-icons/bs";
+type inputProps = {note:INote}
+
+const NoteMin = ({note}:inputProps) => {
+    const parseNoteDate = (content:string) => { // TODO - replace with model method
+        if(!content) return "";
+        const dates:Array<string> = [];
+        const match = content.match(dateFormats)
+            match?.forEach(date => {
+                let formattedDate = date.replaceAll('-', '/');
+                formattedDate = date.replaceAll('.', '/');
+                dates.push(formattedDate);
+            })
+        return dates.toString();
+    };
+    const handleNoteClick:MouseEventHandler<HTMLDivElement> = (e) => {
+        console.log("note click", e)
+    };
+    const handleEdit:MouseEventHandler<HTMLDivElement> = (e) => {
+        console.log("edit note", e)
+    };
+    const handleArchive:MouseEventHandler<HTMLDivElement> = (e) => {
+        console.log("archive note", e)
+    };
+    const handleDelete:MouseEventHandler<HTMLDivElement> = (e) => {
+        console.log("delete note", e)
+    };
+    return (
+        <tr className="bg-blue-200 rounded" onClick={handleNoteClick}>
+            <th className="text-center h-14 rounded-l">{1}</th>
+            <td>{note.name}</td>
+            <td>{note.createDate.toLocaleDateString("en-GB")}</td>
+            <td>{note.category}</td>
+            <td>{note.content}</td> 
+            <td>{parseNoteDate(note.content ?? "")}</td>
+            <td className="text-end rounded-r">
+                <BsPencil className="inline w-10 h-10 p-1" onClick={handleEdit}/>
+                <BsArchive className="inline w-10 h-10 p-1" onClick={handleArchive}/>
+                <BsBackspace className="inline w-10 h-10 p-1" onClick={handleDelete}/>
+            </td>
+        </tr>
+    )
+}
+
+export default NoteMin;
