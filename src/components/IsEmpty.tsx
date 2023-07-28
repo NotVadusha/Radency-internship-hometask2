355cdf6ae4348edad3fe917f0cc481d01861ffcd
NotVadusha <vadimkbondarchuk@gmail.com>
2023-07-28 18:39:39 +0300

    feat: add error warnings

--- src/components/IsEmpty.tsx
+++ src/components/IsEmpty.tsx
@@ -1,8 +1,11 @@
 function IsEmpty() {
   return (
-    <div>
-      <p>Empty</p>
-    </div>
+    <tr className="w-full">
+      <td colSpan={7}>
+        <h1 className="text-center text-4xl my-4">Empty :(</h1>
+        <p className="text-center text-3xl my-4">Let's add some notes here!</p>
+      </td>
+    </tr>
   );
 }
 
